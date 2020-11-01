b79e1f3580c8735c1d0e9e14cc8d0a8acafd774e
zfankai <1982614192@qq.com>
2020-11-01 14:19:26 +0800

    uu

---
+++ kaggle-house-prices/code/data-process.py
@@ -0,0 +1,26 @@
+# %matplotlib inline
+import torch
+import torch.nn as nn
+import numpy as np
+import pandas as pd
+import sys
+
+sys.path.append("..")
+
+print(torch.__version__)
+torch.set_default_tensor_type(torch.FloatTensor)
+
+train_data = pd.read_csv('../data/train.csv')
+test_data = pd.read_csv('../data/test.csv')
+print(train_data.shape)
+test_data.shape
+
+a = [1, 2, 3, 'NaN']
+b = ["a", "b", "c", "NaN"]
+df = pd.DataFrame({"A": a, "B": b})
+mm = df.dtypes.index
+print(mm)
+print("===")
+numerical_index = df.dtypes[df.dtypes != 'object'].index
+print(df.dtypes)
+print(numerical_index)
